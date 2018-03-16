2e908d13bb0a33df3fc46b13e7ac1268a4e140f8
asmac89 <asmachiknawari@hotmail.com>
2018-03-16 15:30:48 -0400

    asma

--- src/test/java/skyware_test/pages/DashBoardPage.java
+++ src/test/java/skyware_test/pages/DashBoardPage.java
@@ -1,6 +1,8 @@
 package skyware_test.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DashBoardPage {
@@ -11,6 +13,49 @@ public class DashBoardPage {
 		PageFactory.initElements(driver, this);
 	}
 	
+	@FindBy(id="thirdtabLink")
+	public WebElement categoryTab;
 	
+	@FindBy(id="fourthtabLink")
+	public WebElement categorySubTab;
+	
+	@FindBy(id="newCategoryButton")
+	public WebElement createNewCategory;
+	
+	@FindBy(id="name")
+	public WebElement categoryName;
+	
+	@FindBy(id="saveButton")
+	public WebElement save;
+	
+	@FindBy(id="searchTerm")
+	public WebElement searchBoxCategories;
+	
+	@FindBy(id="nameTxt")
+	public WebElement searchItem;
+	
+	@FindBy(xpath="(//input[@class='editButton'])[2]")
+	public WebElement editButton;
+	
+	@FindBy(id="profile_firstName")
+	public WebElement firstName;
+	
+	@FindBy(id="profile_lastName")
+	public WebElement lastName;
+	
+	@FindBy(xpath="(//input[@class='saveButton'])[2]")
+	public WebElement saveButton;
+	
+	@FindBy(xpath="(//span[@class='placeholder'])[4]")
+	public WebElement savedFirstName;
+	
+	@FindBy(xpath="(//span[@class='placeholder'])[5]")
+	public WebElement savedLastName;
+	
+	@FindBy(id="subtabLink")
+	public WebElement logoutButton;
+	
+	@FindBy(id="subtabLink")
+	public WebElement logoutMessage;
 	
 }
