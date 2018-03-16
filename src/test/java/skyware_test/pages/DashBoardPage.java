b9dd48f687926180a509560a3f60e9fe7a692226
Ilia Stepanov <ilja.ste@yandex.ru>
2018-03-16 14:52:16 -0400

    Ilya

--- src/test/java/skyware_test/pages/DashBoardPage.java
+++ src/test/java/skyware_test/pages/DashBoardPage.java
@@ -1,5 +1,16 @@
 package skyware_test.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class DashBoardPage {
 
+	WebDriver driver;
+	public DashBoardPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
 }
